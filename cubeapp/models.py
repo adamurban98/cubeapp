1e63bbdf41270969e3ae31939f902b3d0d12fb08
adamurban98 <adamurban98@gmail.com>
2018-10-08 07:54:30 +0100

    Basic models added

--- cubeapp/models.py
+++ cubeapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class RealObject(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name = "RealObject"
+        verbose_name_plural = "RealObjects"
+
+    def __str__(self):
+        return self.name
+
+
+class Abbreviation(models.Model):
+    first = models.CharField(max_length=1)
+    second = models.CharField(max_length=1)
+    real_objects = models.ManyToManyField(RealObject)
+
+    class Meta:
+        verbose_name = "Abbreviation"
+        verbose_name_plural = "Abbreviations"
+
+    def __str__(self):
+        return (self.first + self.second).upper()
