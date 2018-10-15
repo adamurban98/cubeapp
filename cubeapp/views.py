8e365fd3e8282bd8b966bc79af71ba276e160f46
adamurban98 <adamurban98@gmail.com>
2018-10-15 09:30:39 +0100

    Show cards from database

--- cubeapp/views.py
+++ cubeapp/views.py
@@ -2,11 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.views import LoginView
+from .models import Abbreviation
 from . import logout_success
 
 
 def index(request):
-    context = {"request": request}
+    cards = []
+    for a in Abbreviation.objects.all():
+        card = {}
+        card['abbreviation'] = a
+        card['realobjects'] = a.realobject_set.all()
+        cards.append(card)
+    context = {'request': request, 'cards': cards}
     return render(request, 'cubeapp/index.html', context)
 
 
