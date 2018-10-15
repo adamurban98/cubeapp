048f7e2582b267458792af682250ca61b4482a6f
adamurban98 <adamurban98@gmail.com>
2018-10-15 06:39:53 +0100

    Login succes messages added

--- cubeapp/logout_success.py
+++ cubeapp/logout_success.py
@@ -2,13 +2,13 @@ from django.contrib.auth.signals import user_logged_out, user_login_failed
 from django.contrib import messages
 
 
-def show_message(sender, user, request, **kwargs):
+def show_message_logout_succes(sender, user, request, **kwargs):
     messages.success(request, 'You have been successfully logged out.')
 
 
-def show_message2(sender, user, request, **kwargs):
-    messages.success(request, 'Login failed')
+def show_message_login_fail(sender, credentials, request, **kwargs):
+    messages.success(request, 'Login failed. Incorrect username or password.')
 
 
-user_logged_out.connect(show_message)
-user_login_failed.connect(show_message2)
+user_logged_out.connect(show_message_logout_succes)
+user_login_failed.connect(show_message_login_fail)
