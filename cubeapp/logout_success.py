e1c3588986671efa52269f29451442d190c2c5cd
Adam Johnson <me@adamj.eu>
2018-11-19 23:13:38 +0000

    Fix some small typos
    These are all cosmetic *apart* from the fix for the method name `get_success_message`, which was stopping the message from appearing. The default message is empty, and the mixin doesn't post the empty message ([source](https://github.com/django/django/blob/b9cf764be62e77b4777b3a75ec256f6209a57671/django/contrib/messages/views.py#L13)), so nothing was happening. Python doesn't really have a mechanism for automatically catching such typos - you can add any method you want on any subclass, no problem. Really the way to catch this kind of bug is with some testing, automated or manual :) :)

--- cubeapp/logout_success.py
+++ cubeapp/logout_success.py
@@ -2,7 +2,7 @@ from django.contrib.auth.signals import user_logged_out, user_login_failed
 from django.contrib import messages
 
 
-def show_message_logout_succes(sender, user, request, **kwargs):
+def show_message_logout_success(sender, user, request, **kwargs):
     messages.success(request, 'You have been successfully logged out.')
 
 
@@ -10,5 +10,5 @@ def show_message_login_fail(sender, credentials, request, **kwargs):
     messages.success(request, 'Login failed. Incorrect username or password.')
 
 
-user_logged_out.connect(show_message_logout_succes)
+user_logged_out.connect(show_message_logout_success)
 user_login_failed.connect(show_message_login_fail)
