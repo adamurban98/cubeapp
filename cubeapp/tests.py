a0cd7d3d3a8b28df91dd9c8a63bb9fdcef2b7a91
Adam Urban <adamurban98@gmail.com>
2019-02-17 15:39:09 +0000

    Intentionally add a failing test

--- cubeapp/tests.py
+++ cubeapp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class DummyTestCase(TestCase):
+
+    def test_something(self):
+        self.assertEqual(1, 1)
+    def test_something_else(self):
+        self.assertEqual(0, 1)
