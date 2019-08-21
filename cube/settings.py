70e459cd0df4cc99c39f701ec878318d11c2819b
Adam Urban <adamurban98@gmail.com>
2019-08-21 13:19:13 +0100

    change local testing db settings

--- cube/settings.py
+++ cube/settings.py
@@ -75,10 +75,16 @@ WSGI_APPLICATION = 'cube.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+
+# testing locally
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE':   'django.db.backends.postgresql_psycopg2',
+        'NAME':     'mydb',
+        'USER':     'postgres',
+        'PASSWORD': 'postgres',
+        'HOST':     'localhost',
+        'PORT':     '',
     }
 }
 
